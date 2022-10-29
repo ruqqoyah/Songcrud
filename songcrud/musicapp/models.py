0df8003a21aa2c06b6d56510ac6b1647a820bce4
ruqqoyah <testingna22@gmail.com>
2022-10-28 23:39:10 -0400

    Creating models, attributes and relationships between the models.

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    Age = models.PositiveSmallIntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.PositiveIntegerField()
+    artiste_id = models.IntegerField()
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1500)
+    song_id = models.IntegerField()
